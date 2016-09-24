a03edca59d3d60908fbbcd2115de5a81ce5fc7b2
Kylie Soderblom <MacDream@Kylies-MacBook-Pro.local>
2016-09-23 17:51:10 -0700

    unable to pull ItemChoice value all the through to Order function where StockQuantity needs to update but doesn't function YET. Table formatting and small details - number validation still need to be fixed. BamazonManager -in progress. Hope to create third transaction table to complete #3 challenge later this evening.

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -8,6 +8,8 @@
 var mysql = require('mysql');
 var prompt = require('prompt');
 var colors = require('colors/safe');
+var inquirer = require('inquirer');
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -34,85 +36,115 @@ connection.connect(function(err) {
 
 })
 
+
 display();
 
 function display() {
     connection.query("SELECT * FROM products", function(err, res) {
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].ItemID + " | " + res[i].ProductName + " | " + "$" + res[i].Price);
+            console.log("\n" + res[i].ItemID + " | " + res[i].ProductName + " | " + "$" + res[i].Price);
             console.log("*******************************************************");
         }
-    })
-    initPrompt();
+	    
+    }).then
+    	 initPrompt();
 };
+   
+// };
 
-// initPrompt();
 
-// .then -- initprompt
-
-prompt.message = colors.bold.blue("Question!");
-prompt.delimiter = colors.green("");
+// prompt.message = colors.bold.blue("Question!");
+// prompt.delimiter = colors.green("");
 
 
 function initPrompt() {
     prompt.get({
         properties: {
             item: {
-                description: colors.blue("Please enter the ItemID of the product you wish to buy."),
+                description: colors.bold.blue("\n \n Please enter the ItemID of the product you wish to buy."),
                 // if isNAN (item) ==false, return true, else 
                 message: colors.red("Please enter a valid item number.")
             },
             quantity: {
-                description: colors.blue("How many would you like to buy?"),
+                description: colors.bold.blue("How many would you like to buy?"),
                 // if isNAN (item) ==false, return true, else 
                 message: colors.red("Please enter a valid number. Max. 10 products per customer.")
             }
         }
     }, function(err, res) {
-        console.log(res);
+        // console.log(res);
         itemChoice = res.item; 
         howMany = res.quantity;
-        console.log(howMany);
-        console.log(itemChoice);
+        // console.log(howMany);
+        // console.log(itemChoice);
         checkQty(itemChoice, howMany);
         // pull values itemID, product name, price, (var howMany, itemPrice, itemChoice,)
-    })
-}
+    	})
+	
+};
 
 
 // 
 function checkQty(itemChoice, howMany) {
     connection.query("SELECT StockQuantity, Price, ProductName FROM products WHERE ? ", { ItemID: itemChoice }, function(err, result) {
         var product = result[0].ProductName;
-        console.log(colors.bold.green("You chose " + howMany + "  " + product + "(s)"));
+        console.log(colors.bold.blue("You chose " + howMany + "  " + product + "(s)"));
 
-        var currentQty = result.StockQuantity;
+        var currentQty = result[0].StockQuantity;
+        console.log(colors.bold.green("There are currently  " + currentQty + " " + product + "(s) in stock"));
         itemPrice = result[0].Price;
         totalCost = howMany * itemPrice;
 
         if (currentQty <= 0 || currentQty < howMany) {
             console.log(colors.bold.red("Insufficient Quantity"));
-            console.log(colors.orange("There are only " + currentQty + "available."));
+            console.log(colors.bold.red("There are only " + currentQty + " available."));
             console.log("Please choose a smaller quantity or another item");
-            display();
+            // display();
             initPrompt();
         } else {
-            console.log(colors.bold.blue("Yes, we have " + howMany + " in stock."));
-            orderProduct();
+            // console.log(colors.bold.blue("Yes, we have " + howMany + " in stock."));
             newQty = currentQty - howMany;
+            // console.log("Does new Qty calculate correctly? " + newQty);
+            orderProduct(itemChoice, newQty);
 
         }
-    });
-};
-
-function orderProduct(itemChoice) {
-    connection.query("UPDATE products SET StockQuantity = newQty WHERE ? ", { ItemID: itemChoice }, function(err, res) {
-        console.log(colors.bold.blue("The total cost of your order is  $ " + totalCost));
-        console.log(colors.bold.green("Your product(s) will be shipped to you shortly"));
-        // display();
-        // initPrompt(); 
     })
 };
+
+function orderProduct(itemChoice, newQty) {
+        // console.log("The newQty variable from above function works " + newQty);
+
+    connection.query("UPDATE products SET StockQuantity = newQty WHERE ? ", { ItemID: itemChoice}, function(err, res) {
+        console.log(colors.bold.blue("The total cost of your order is  $ " + totalCost));
+        console.log(colors.bold.green("Your product(s) will be shipped to you shortly"));
+        console.log(colors.bold.green("After you order is shipped, there will be " + newQty + " in stock"));
+        moreStuff();
+})
+}
+
+function moreStuff(){
+	inquirer.prompt([{
+        type: "confirm",
+        message: "Would you like to purchase additional items? (Y/n)",
+        name: "moreStuffQ"
+	}]).then (function (yes){
+		if (yes.moreStuffQ) {
+			initPrompt();
+        }else{
+         console.log(colors.bold.blue("Thank you for your business. We look forward to seeing you again!"));
+         console.log(colors.italic("Our current programmer does not yet know how to code a graceful exit. Please type control+c "));
+        } 
+    // })
+});
+}
+
+
+
+
+
+
+
+
 
 // + " = " + howMany + " x  " + itemChoice.
 
